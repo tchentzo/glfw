0e1ef883ee7e8f63a16c33175ee4969c1ed44c8a
thecro <tchentzo@gmail.com>
2022-10-03 17:30:27 +0200

    including all necessary files to build out

--- premake5.lua
+++ premake5.lua
@@ -15,31 +15,33 @@ project "GLFW"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/platform.c",
+		"platform.h",
+		"internal.h",
+		"win32_module.c",
+		"null_platform.h",
+		"null_init.c",
+		"null_monitor.c",
+		"null_window.c",
+		"null_joystick.c",
+		"null_joystick.h",
+
+
+		"src/win32_init.c",
+		"src/win32_joystick.c",
+		"src/win32_monitor.c",
+		"src/win32_time.c",
+		"src/win32_thread.c",
+		"src/win32_window.c",
+		"src/wgl_context.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		staticruntime "On"
-
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
 
 	filter "configurations:Debug"
 		runtime "Debug"
